ac295d092ca1f647d9715c745e6079cfbfaabcfa
Oshurkov Oleg <wirc1990@mail.ru>
2022-06-08 17:21:09 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Oshurkov project
---
+++ src/main/java/ru/javarush/oshurkov/planetofanimal/Runner.java
@@ -0,0 +1,7 @@
+package ru.javarush.oshurkov.planetofanimal;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello maven!");
+    }
+}
